b955865ec956ed9859a48fc5fe4811b7b337f7fd
intunemdmvn <201134654+intunemdmvn@users.noreply.github.com>
2025-07-08 20:44:54 +0700

    Update Convert-PowerShellToBatch.ps1

--- Convert-PowerShellToBatch.ps1
+++ Convert-PowerShellToBatch.ps1
@@ -1,1 +1,14 @@
+function Convert-PowerShellToBatch {
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Path
+    )
+    process {
+        $encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes((Get-Content -Path $Path -Raw -Encoding UTF8)))
+        $newPath = [Io.Path]::ChangeExtension($Path, ".bat")
+        "@echo off`n`npowershell.exe -NoExit -encodedCommand $encoded" | Set-Content -Path $newPath -Encoding ASCII
+    }
+}
 
+Convert-PowerShellToBatch -Path "C:\Path\To\YourScript.ps1"
